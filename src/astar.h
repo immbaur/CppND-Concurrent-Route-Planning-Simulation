08497081b31ca55d300acc150e6f6c47d956c058
Immanuel Baur <immanuel.baur@daimler.com>
2020-04-04 13:50:05 -0700

    implementing maphandle and astar in source file instead of inline in header

--- src/astar.h
+++ src/astar.h
@@ -7,141 +7,31 @@
 
 #include "mystructs.h"
 
-using std::cout;
-using std::ifstream;
-using std::istringstream;
-using std::sort;
-using std::string;
-using std::vector;
-using std::abs;
-
-// directional deltas
-const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
-
-/**
-* Compare the F values of two cells.
-*/
-bool Compare(const vector<int> a, const vector<int> b) {
-  int f1 = a[2] + a[3]; // f1 = g1 + h1
-  int f2 = b[2] + b[3]; // f2 = g2 + h2
-  return f1 > f2;
-}
-
 class AStar{
 
 public:
   /**
    * Implementation of A* search algorithm
    */
-  bool Search(vector<vector<State>> grid, std::vector<std::vector<Position>> &path, int init[2], int goal[2]) {
-    // Create the vector of open nodes.
-    vector<vector<int>> open {};
-
-    // Initialize the starting node.
-    int x = init[0];
-    int y = init[1];
-    int x_pre = x;
-    int y_pre = y;
-    int g = 0;
-    int h = Heuristic(x, y, goal[0],goal[1]);
-    AddToOpen(x, y, g, h, open, grid, x, y);
-
-    while (open.size() > 0) {
-      // Get the next node
-      CellSort(&open);
-      auto current = open.back();
-      open.pop_back();
-      x = current[0];
-      y = current[1];
-
-      x_pre = current[4];
-      y_pre = current[5];
-
-      grid[x][y] = State::kPath;
-
-      // std::cout << "path: " << x << " " << y << std::endl;
-
-      // std::vector<Position> vec = {{(double)y,(double)x},{(double)y_pre,(double)x_pre}};
-      path.push_back({{(double)y,(double)x},{(double)y_pre,(double)x_pre}});
-
-      // Check if we're done.
-      if (x == goal[0] && y == goal[1]) {
-        grid[init[0]][init[1]] = State::kStart;
-        grid[goal[0]][goal[1]] = State::kFinish;
-        return true;
-      }
-
-      // If we're not done, expand search to current node's neighbors.
-      ExpandNeighbors(current, goal, open, grid);
-    }
-
-    // We've run out of new nodes to explore and haven't found a path.
-    cout << "No path found!" << "\n";
-    return false;
-  }
+  bool Search(std::vector<std::vector<State>> grid, std::vector<std::vector<Position>> &path, int init[2], int goal[2]);
 
 private:
   /**
    * Sort the two-dimensional vector of ints in descending order.
    */
-  void CellSort(vector<vector<int>> *v) {
-    sort(v->begin(), v->end(), Compare);
-  }
-
-
+  void CellSort(std::vector<std::vector<int>> *v);
   // Calculate the manhattan distance
-  int Heuristic(int x1, int y1, int x2, int y2) {
-    return abs(x2 - x1) + abs(y2 - y1);
-  }
-
-
+  int Heuristic(int x1, int y1, int x2, int y2);
   /**
    * Check that a cell is valid: on the grid, not an obstacle, and clear.
    */
-  bool CheckValidCell(int x, int y, vector<vector<State>> &grid) {
-    bool on_grid_x = (x >= 0 && x < grid.size());
-    bool on_grid_y = (y >= 0 && y < grid[0].size());
-    // std::cout << "check valid: " << x << " " << y << '\n';
-    if (on_grid_x && on_grid_y){
-      // std::cout << "valid: " << static_cast<std::underlying_type<State>::type>(grid[x][y]) << '\n';
-      return grid[x][y] == State::kEmpty;
-    }
-    return false;
-  }
-
-
+  bool CheckValidCell(int x, int y, std::vector<std::vector<State>> &grid);
   /**
    * Add a node to the open list and mark it as open.
    */
-  void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openlist, vector<vector<State>> &grid, int x_pre, int y_pre) {
-    // Add node to open vector, and mark grid cell as closed.
-    openlist.push_back(vector<int>{x, y, g, h, x_pre, y_pre});
-    grid[x][y] = State::kClosed;
-  }
-
-
+  void AddToOpen(int x, int y, int g, int h, std::vector<std::vector<int>> &openlist, std::vector<std::vector<State>> &grid, int x_pre, int y_pre);
   /**
    * Expand current nodes's neighbors and add them to the open list.
    */
-  void ExpandNeighbors(const vector<int> &current, int goal[2], vector<vector<int>> &openlist, vector<vector<State>> &grid) {
-    // Get current node's data.
-    int x = current[0];
-    int y = current[1];
-    int g = current[2];
-
-    // Loop through current node's potential neighbors.
-    for (int i = 0; i < 4; i++) {
-      int x2 = x + delta[i][0];
-      int y2 = y + delta[i][1];
-
-      // Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
-      if (CheckValidCell(x2, y2, grid)) {
-        // Increment g value and add neighbor to open list.
-        int g2 = g + 1;
-        int h2 = Heuristic(x2, y2, goal[0], goal[1]);
-        AddToOpen(x2, y2, g2, h2, openlist, grid, x, y);
-      }
-    }
-  }
-
+  void ExpandNeighbors(const std::vector<int> &current, int goal[2], std::vector<std::vector<int>> &openlist, std::vector<std::vector<State>> &grid);
 };
--- src/maphandle.h
+++ src/maphandle.h
@@ -5,88 +5,33 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <mutex>
 
 #include "mystructs.h"
 
 class Map{
 
 public:
+  // public member functions
+  Map(std::string path);
 
-  Map(std::string path){
-    std::lock_guard<std::mutex> lck(_map_mutex);
-    _map.clear();
-    std::ifstream myfile (path);
-    if (myfile) {
-      std::string line;
-      while (getline(myfile, line)) {
-        std::vector<State> row = parseLine(line);
-        _map.push_back(row);
-      }
-    } else {
-      std::cerr << "File not found!" << std::endl;
-    }
-    // set the row size and column size of the map
-    rowSize = _map.size();
-    colSize = _map[1].size();
-  };
+  void reservePathInMap(std::vector<Position> &path);
 
-  void reservePathInMap(std::vector<Position> &path){
-    // std::cout << "reserve Path:" << std::endl;
-    std::lock_guard<std::mutex> lck(_map_mutex);
-    for(auto it = path.begin(); it != path.end(); ++it) {
-      _map[(*it).y][(*it).x] = State::kClosed;
-      // std::cout << "X: " << (*it).x << "Y: " << (*it).y << std::endl;
-    }
-  }
+  void freeField(Position &pos);
 
-  void freeField(Position &pos){
-    std::lock_guard<std::mutex> lck(_map_mutex);
-    _map[pos.y][pos.x] = State::kEmpty;
-    // std::cout << "Free field X: " << pos.x << "Y: " << pos.y << std::endl;
-  }
+  std::vector<std::vector<State>> getMap();
 
-  std::vector<std::vector<State>> getMap(){
-    std::lock_guard<std::mutex> lck(_map_mutex);
-    std::vector<std::vector<State>> temp = _map;
-    return std::move(temp);
-  };
-
-  Position getRandPos(bool reserve){
-    State state = State::kObstacle;
-    Position pos;
-    std::lock_guard<std::mutex> lck(_map_mutex);
-    while (state != State::kEmpty){
-      pos.x = rand()%colSize;
-      pos.y = rand()%rowSize;
-      state = _map[pos.y][pos.x];
-    }
-    if(reserve)
-      _map[pos.y][pos.x] = State::kClosed;
-    return pos;
-  }
+  Position getRandPos(bool reserve);
   // publick memeber variables
   int rowSize;
   int colSize;
 
 private:
   // private member functions
-  std::vector<State> parseLine(std::string line) {
-    std::istringstream sline(line);
-    int n;
-    char c;
-    std::vector<State> row;
-    while (sline >> n >> c && c == ',') {
-      if (n == 0) {
-        row.push_back(State::kEmpty);
-      } else {
-        row.push_back(State::kObstacle);
-      }
-    }
-    return row;
-  }
-
+  std::vector<State> parseLine(std::string line);
+  // private member variables
   std::vector<std::vector<State>> _map;
-  // mutex for reading writing the map
+  // mutex for save reading and writing to the map
   std::mutex _map_mutex;
 };
 
